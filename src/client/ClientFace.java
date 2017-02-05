0b67bfe04f93cab216589ec9249522251f4ca563
veivan <veivan@yandex.ru>
2017-02-06 00:30:46 +0300

    New import

--- src/client/ClientFace.java
+++ src/client/ClientFace.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.FileNotFoundException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
